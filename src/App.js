2708c4d289f1e8a531f47c730ddd17f0626be61a
RajeshChinta <chinta.lakshmirajesh@gmail.com>
2025-06-04 20:35:51 +0530

    Prepared for Deployment

--- src/App.js
+++ src/App.js
@@ -1,3 +1,22 @@
+// import React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import InvoiceTable from "./InvoiceTable";
+// import AddInvoiceForm from "./AddInvoiceForm";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<InvoiceTable />} />
+//         <Route path="/add" element={<AddInvoiceForm />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
+// export default App; 
+
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import InvoiceTable from "./InvoiceTable";
@@ -5,7 +24,7 @@ import AddInvoiceForm from "./AddInvoiceForm";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/SheetStockr">
       <Routes>
         <Route path="/" element={<InvoiceTable />} />
         <Route path="/add" element={<AddInvoiceForm />} />
@@ -14,4 +33,4 @@ function App() {
   );
 }
 
-export default App; 
+export default App;
