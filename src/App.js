e5c9ca147da3c302e1de2764b630cf8fa05f4fdc
RajeshChinta <chinta.lakshmirajesh@gmail.com>
2025-06-04 21:01:18 +0530

    new update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,4 @@
-// import React from "react";
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import InvoiceTable from "./InvoiceTable";
-// import AddInvoiceForm from "./AddInvoiceForm";
 
-// function App() {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/" element={<InvoiceTable />} />
-//         <Route path="/add" element={<AddInvoiceForm />} />
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App; 
-
-//hi 
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import InvoiceTable from "./InvoiceTable";
